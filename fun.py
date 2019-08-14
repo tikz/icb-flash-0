5979a274b5a0d2968be214f5bbd7a02a3bd57e9d
Mauro Song <tik@lag.party>
2019-08-14 16:17:51 -0300

    Devolver nombre func en benchmarkEstrategia. Verborragia.

--- fun.py
+++ fun.py
@@ -3,61 +3,54 @@ from opcionales import jugarSmart, jugarSmart2
 
 
 def quienGano(lista):
-    # Recibe una lista con las sumas de cada jugador (como la salida de jugar_varios) y devuelve
-    # el index del jugador que gano.
+    # quienGano recibe una lista con las sumas de cada jugador y devuelve el index del jugador que gano.
     # Si no gano nadie, devuelve None.
-    # Si hay empate (ej: los mejores puntajes son dos jugadores que tienen 20) considero que no gano
-    # nadie, y tambien devuelvo None.
+    # Si hay empate entre dos o mas jugadores, tambien devuelve None.
 
     # Primero creo una lista con tuplas que contengan (index, suma)
     l = [(i, x) for i, x in enumerate(lista)]
 
-    # Solo me interesan los que no se pasaron de 21
+    # Solo me interesan los que no se pasaron de 21.
     l = [x for x in l if x[1] <= 21]
 
     if len(l) == 0:
-        return None  # Se pasaron todos de 21
+        return None  # Se pasaron todos de 21, perdieron todos.
     elif len(l) == 1:
-        return l[0][0]  # Hay uno solo que todavia no perdio. Gano ese.
+        return l[0][0]  # Hay uno solo que no perdio por pasarse. Gano ese.
 
     distancias = [(i, 21 - x) for i, x in l]
 
-    # Ordeno la lista de forma creciente en base a la distancia al 21
+    # Ordeno la lista de forma decreciente en base a la distancia al 21.
     distancias.sort(key=lambda x: x[1])
 
-    # El primer jugador de la lista ordenada tiene la misma distancia que el 2do. Hay un empate entre esos dos (o mas).
+    # El primer jugador de la lista ordenada tiene la misma distancia que el 2do. Hay un empate entre esos dos (o mas jugadores).
     if distancias[0][1] == distancias[1][1]:
         return None
     else:
         return distancias[0][0]
 
 
-def benchmarkEstrategia(lista_jug):
-    # benchmarkEstrategia recibe una lista de jugadores al igual que compararEstrategia
-    # pero realiza 1000 partidas y devuelve una lista con el winrate de cada jugador
-
-    # 0=jugar, 1=jugarMiedo, 2=jugarBorracho, 3=jugarSmart, 4=jugarSmart2
-    funcs = [jugar, jugarMiedo, jugarBorracho, jugarSmart, jugarSmart2]
+def benchmarkEstrategia(lista_funcs, n=1000):
+    # benchmarkEstrategia recibe una lista de funciones estilo jugar(), realiza n partidas
+    # y devuelve una lista con tuplas (nombreFuncion, winrate).
 
     ganadores = []
 
-    n = 1000
-
     print(f"Haciendo {n} partidas con un mazo nuevo cada una...")
     for _ in range(n):
-        mazo = generarMazo(len(lista_jug))
+        mazo = generarMazo(len(lista_funcs))
 
         puntajes = []
-        for j in lista_jug:
-            puntajes.append(funcs[j](mazo))
+        for f in lista_funcs:
+            puntajes.append(f(mazo))
         ganadores.append(quienGano(puntajes))
 
     winrates = []
-    for jug in lista_jug:
+    for i, _ in enumerate(lista_funcs):
         wins = 0
         for g in ganadores:
-            if jug == g:
+            if i == g:
                 wins += 1
         winrates.append(wins / n)
 
-    return winrates
+    return [(func.__name__, winrate) for func, winrate in zip(lista_funcs, winrates)]
